e0322dfa09dd64420633c3410e1deaac8fcabf21
arocasan <mr@michelantonio.com>
2023-07-12 13:15:19 +0200

    WAR-12 War over!

--- war.py
+++ war.py
@@ -67,4 +67,82 @@ class Player:
 player_one = Player("Mika")
 player_two = Player("Louie")
 
-#Just to push another thing
+# Setting up players
+new_deck = Deck()
+# Shuffle deck
+new_deck.shuffle()
+
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+
+round_num = 0 
+
+
+while game_on:
+
+    round_num += 1
+    print(f"Round {round_num}")
+
+
+    if len(player_one.all_cards) == 0:
+        print(f"{player_one}, out of cards! {player_two} wins!")
+        game_on = False
+        break
+
+    
+    if len(player_two.all_cards) == 0:
+        print(f"{player_two}, out of cards! {player_one} wins!")
+        game_on = False
+        break
+
+
+    # Start a new round 
+
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            at_war = False
+                         
+        else:
+            print(f"There's a WAR going on between {player_one} and {player_two}")
+
+            if len(player_one.all_cards) < 5:
+                print(f"{player_one} unable to declare war")
+                print(f"{player_two} WINS!")
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 5:
+                print(f"{player_two} unable to declare war")
+                print(f"{player_one} WINS!")
+                game_on = False
+                break
+
+            else:
+
+                for num in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
